842352c106bd382053302c4024d7777b62bff6f1
HY <newlovenewworld2@gmail.com>
2022-06-02 18:46:14 +0900

    create and edit css done

--- my-project/resources/views/create.blade.php
+++ my-project/resources/views/create.blade.php
@@ -6,26 +6,29 @@
         <title></title>
     </head>
     <body>
-        <a class="btn return_list" href="{{ url('/list') }}">一覧に戻る</a>
 
-        <form action="/create" method="get">
-            <p>タスク名</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('task_name') }}</p>
-            @endif
-            <input name="task_name" class="taskname" type="text">
-            <p>期日</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('deadline') }}</p>
-            @endif
-            <input name="deadline" type="date">
-            <p>備考</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('remarks') }}</p>
-            @endif
-            <textarea name="remarks" class="tasktxt" cols="50" rows="25"></textarea>
-            <input type="submit" class="btn return_list" value="登録">
-        </form>
+        <div class="main">
+            <a class="btn return_list" href="{{ url('/list') }}">一覧に戻る</a>
+
+            <form action="/create" method="get">
+                <p>タスク名</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('task_name') }}</p>
+                @endif
+                <input name="task_name" class="taskname" type="text">
+                <p>期日</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('deadline') }}</p>
+                @endif
+                <input name="deadline" type="date">
+                <p>備考</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('remarks') }}</p>
+                @endif
+                <textarea name="remarks" class="tasktxt" cols="50" rows="25"></textarea>
+                <input type="submit" class="btn return_list" value="登録">
+            </form>
+        </div>
 
     </body>
 </html>
--- my-project/resources/views/edit.blade.php
+++ my-project/resources/views/edit.blade.php
@@ -7,26 +7,28 @@
     </head>
     <body>
 
-        <a class="btn return_list" href="{{ url('/list') }}">一覧に戻る</a>
+        <div class="main">
+            <a class="btn return_list" href="{{ url('/list') }}">一覧に戻る</a>
 
-        <form action="/edit/{{ $id }}" method="get">
-            <p>タスク名</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('task_name') }}</p>
-            @endif
-            <input name="task_name" class="taskname" type="text" value="{{ $task_name }}">
-            <p>期日</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('deadline') }}</p>
-            @endif
-            <input name="deadline" type="date" value="{{ $deadline }}">
-            <p>備考</p>
-            @if($errors->any())
-                <p class="error-message">{{ $errors->first('remarks') }}</p>
-            @endif
-            <textarea name="remarks" class="tasktxt" cols="50" rows="25">{{ $remarks }}</textarea>
-            <input type="submit" class="btn return_list" value="更新">
-        </form>
+            <form action="/edit/{{ $id }}" method="get">
+                <p>タスク名</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('task_name') }}</p>
+                @endif
+                <input name="task_name" class="taskname" type="text" value="{{ $task_name }}">
+                <p>期日</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('deadline') }}</p>
+                @endif
+                <input name="deadline" type="date" value="{{ $deadline }}">
+                <p>備考</p>
+                @if($errors->any())
+                    <p class="error-message">{{ $errors->first('remarks') }}</p>
+                @endif
+                <textarea name="remarks" class="tasktxt" cols="50" rows="25">{{ $remarks }}</textarea>
+                <input type="submit" class="btn return_list" value="更新">
+            </form>
+        </div>
 
     </body>
 </html>
